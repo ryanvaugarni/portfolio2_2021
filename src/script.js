7ee4d70b2800222bd5f66b4e8a7a9e87179549cb
Vaugarni ryan <ryan.vaugarni@gmail.com>
2021-11-03 14:31:37 +0100

    first

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("Vous êtes connecté")
     let date, foot
+    let menu = document.querySelector("header img");
+    let menu_div = document.querySelector(".menuhidden")
+    menu.addEventListener("click",()=>{
+        menu_div.classList.toggle("menushown")
+
+    })
     date = new Date();
     foot = document.querySelector("footer p");
     foot.innerText += `${date.getFullYear()} - Ryan Vaugarni | All rights reserved`
