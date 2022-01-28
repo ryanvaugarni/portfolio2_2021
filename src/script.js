ea8c67c0d8b7f6ef56381008182365891c7704b9
Vaugarni ryan <ryan.vaugarni@gmail.com>
2022-01-28 17:03:05 +0100

    update

--- src/script.js
+++ src/script.js
@@ -2,10 +2,48 @@ document.addEventListener("DOMContentLoaded",()=>{
     console.log("Vous êtes connecté")
     let date, foot
     let menu = document.querySelector("header img");
-    let menu_div = document.querySelector(".menuhidden")
+    let menu_div = document.querySelector(".hide_menu")
+    let butta = document.querySelector(".aza");
+    let buttb = document.querySelector(".azb");
+    let buttc = document.querySelector(".azc");
+    let buttd = document.querySelector(".azd");
+    let butte = document.querySelector(".aze");
 
+    let buttf = document.querySelector(".aze");
+
+    butta.addEventListener("click",()=>{
+        menu_div.classList.toggle("show_menu")
+        menu.src = "./asset/menu1.png"
+        
+
+    })
+    buttb.addEventListener("click",()=>{
+        menu_div.classList.toggle("show_menu")
+        menu.src = "./asset/menu1.png"
+
+    })
+    buttc.addEventListener("click",()=>{
+        menu_div.classList.toggle("show_menu")
+        menu.src = "./asset/menu1.png"
+
+    })
+    buttd.addEventListener("click",()=>{
+        menu_div.classList.toggle("show_menu")
+        menu.src = "./asset/menu1.png"
+
+    })
+    butte.addEventListener("click",()=>{
+        menu_div.classList.toggle("show_menu")
+        menu.src = "./asset/menu1.png"
+
+    })
+    buttf.addEventListener("click",()=>{
+        menu_div.classList.toggle("show_menu")
+        menu.src = "./asset/menu1.png"
+
+    })
     menu.addEventListener("click",()=>{
-        menu_div.classList.toggle("menushown")
+        menu_div.classList.toggle("show_menu")
         if((menu.src.match("./asset/menu1.png")))
         {
             menu.src = "./asset/croix.png"
