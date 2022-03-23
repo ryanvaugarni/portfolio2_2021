d4ca61cbe694924d64e87669a2081716bbf17bdd
Vaugarni ryan <ryan.vaugarni@gmail.com>
2022-03-23 17:31:38 +0100

    update hamburger and croix

--- src/script.js
+++ src/script.js
@@ -44,12 +44,12 @@ document.addEventListener("DOMContentLoaded",()=>{
     })
     menu.addEventListener("click",()=>{
         menu_div.classList.toggle("show_menu")
-        if((menu.src.match("./asset/menu1.png")))
+        if((menu.src.match("./asset/hamburger.png")))
         {
             menu.src = "./asset/croix.png"
         }
         else{
-            menu.src = "./asset/menu1.png"}
+            menu.src = "./asset/hamburger.png"}
     })
     date = new Date();
     foot = document.querySelector("footer p");
